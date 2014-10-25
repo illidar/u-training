c421b90f2a67f7d3d5af6645d700ca435d22b586
sann26 <sann26@mail.ru>
2014-10-25 23:39:02 +0300

    update cart, fixes

--- skin/frontend/default/urbandecay/js/popup.js
+++ skin/frontend/default/urbandecay/js/popup.js
@@ -1,11 +1,10 @@
 
 
-//Function To Display Popup
 function div_show() {
     document.getElementById('popup').style.display = "block";
     document.getElementById('popupContent').style.display = "block";
 }
-//Function to Hide Popup
+
 function div_hide(){
     document.getElementById('popup').style.display = "none";
     document.getElementById('popupContent').style.display = "none";
@@ -26,3 +25,22 @@ function div_edit_address_hide(){
     document.getElementById('modal-window-address-bg').style.display = "none";
     document.getElementById('modal-window-address').style.display = "none";
 }
+
+window.onscroll = function(){
+    var $j = jQuery.noConflict();
+    var nav = window.scrollY;
+    if (nav > 200) {
+        $j('#up-div').fadeIn(200);
+    } else {
+        $j('#up-div').fadeOut(200);
+    }
+}
+
+function to_Top(){
+    var $j = jQuery.noConflict();
+    $j('body,html').animate({
+        scrollTop: 0
+    }, 500);
+    return false;
+}
+
